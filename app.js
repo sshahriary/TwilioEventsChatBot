68e70bc20bdd21fae5c10d97f3e636a8ec656e90
Sam Shahriary <sshahriary@ucdavis.edu>
2018-11-18 20:27:34 -0800

    further setup - create server file: seperate app from server. db setup: schema, db connection.

--- app.js
+++ app.js
@@ -5,14 +5,29 @@ const twilio = require('twilio');
 const dotenv = require('dotenv').config();      //retrieve local environment variables
 const bodyParser = require('body-parser');      //parse incoming requests
 
+/* node modules */
+const inboundHandler = require('./routes/inbound.js')
 
 
-/* retrieve environment variables */
+/* retrieve environment variables and set variables */
 const accountSID = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const dbUser = process.env.MONGOOSE_USER;
 const dbPass = process.env.MONGOOSE_PASSWORD;
+const mongoURI = 'mongodb://'+dbUser+':'+dbPass+'@ds217351.mlab.com:17351/chatbotdb';
+let messageSchema = new mongoose.Schema({ phoneNumber: String, groupName: String, totalAdults: String, totalKids: String });
 
 /* application setup */
 const app = express();
 const client = new twilio(accountSID, authToken);
+let Message = mongoose.model('Message', messageSchema);                                                     //create model
+app.use(bodyParser.urlencoded({extended: false}));                                                          //format incoming requests
+mongoose.connect(mongoURI, { useNewUrlParser: true }).then( () => {console.log('database connected');});    //connect to mongodb
+
+/* application */
+
+app.get('/', function(req, res, next){
+  res.end();
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,65 @@
+/* npm modules */
+var http = require('http');
+/* node modules */
+var app = require('./app.js');
+
+
+/* get port from environment variable */
+var port = normalizeport('3000');
+/* store port in express */
+app.set('port', port);
+
+/* Listen on provided port, on all network interfaces. */
+var server = http.createServer(app);
+server.listen(port);
+console.log('server connected');
+server.on('error', onError);
+
+module.exports = server;
+
+/*
+** event listener for http server "error" event
+** @param {object} error - specific error event
+*/
+function onError(error){
+
+  if(error.syscall !== 'listen'){
+    throw error;
+  }
+
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+
+  /* handle specific listen error with user friendly message */
+  switch(error.code){
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/*
+** parses string value into integer for port number usage
+** @param {string} value - requested port number
+*/
+function normalizeport(value){
+  var port = parseInt(value, 10);
+
+  if(isNaN(port)){
+    //named pipe
+    return value;
+  }
+
+  if(port >= 0){
+    //port number
+    return port;
+  }
+
+  return false;
+}
